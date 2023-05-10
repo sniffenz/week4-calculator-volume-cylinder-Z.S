c4ca5bbc0965877ac2670e3476dc1ca4adfbb5fa
sniffenz <96217756+sniffenz@users.noreply.github.com>
2023-05-09 14:54:56 -1000

    Update index.js
    Changed the forumla to get the height of a right angle cone

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 //jshint esversion:6
 
-// create an express object from the express package
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
-// create an app object from the express object
+
 const app = express();
-// this allows the parsing of the html file using body parser
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-//this sends the html file to the web page using the root directory
-//
-app.get("/VolCalc", function(req, res) {
-  res.sendFile(__dirname + "/VolCalculator.html")
-});
 
-// this gets the response from the values in the web page
+app.get("/CHCalc", function(req, res) {
+  res.sendFile(__dirname + "/HeightCalculator.html")
+});
 app.post("/", function(req, res){
 
-//use n1 for radium and n2 for height
+
   var num1 = Number(req.body.n1);
   var num2 = Number(req.body.n2);
 
-// does the computation of the input variables, as numbers
-//This is a new comment that is gor GitHub
-  var volCylinder = Math.PI * Math.pow(num1, 2) * num2;
+
+  var height = 3*(num2 / (Math.PI * num1 * num1));
 
 // sends the results back to the web page as string
-  res.send("The volume of the cylinder is " + volCylinder.toFixed(2));
+   res.send("The height of the right circular cone is " + height.toFixed(2));
 })
 //this gets the response from the web page to this placeholder
 
